cef4f3d3650dc8121086bff6bb51b7f5efe356f0
ReinTanto <reinharttanto@gmail.com>
2024-05-24 22:57:17 +1000

    Cart finished, added 404 and coming soon page

--- js/toggle_visibility.js
+++ js/toggle_visibility.js
@@ -5,3 +5,14 @@ function toggleVisibility(divId) {
   var div = document.getElementById(divId);
   div.classList.toggle('show');
 }
+
+//function is used to toggle the visibility of the pages in the cart
+function toggleVisibility2(div1, div2) {
+  var pageToShowElement = document.getElementById(div1);
+  var pageToHideElement = document.getElementById(div2);
+
+  if (pageToShowElement && pageToHideElement) {
+    pageToShowElement.classList.remove('hidden_page');
+    pageToHideElement.classList.add('hidden_page');
+  }
+}
