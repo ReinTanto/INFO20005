fea6272b9388a2f2990826413579996a5ade6186
ReinTanto <reinharttanto@gmail.com>
2024-05-25 18:38:49 +1000

    Created paths to hardcode cart system

--- js/cart_alert.js
+++ js/cart_alert.js
@@ -1,11 +1,10 @@
 //Reinhart Aristeo Tanto - 1376786
 
+//Function for displaying add to cart alert
 document.getElementById("addToCartButton").addEventListener("click", function() {
     var alertMessage = document.getElementById("alertMessage");
     alertMessage.innerText = "Successfully added to cart";
     alertMessage.classList.add("fadeIn");
-
-    // Automatically fade out after 2 seconds
     setTimeout(function() {
         alertMessage.classList.remove("fadeIn");
     }, 1000);
